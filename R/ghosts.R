3b42495b836002e04a96760e07253a99a40fd0f7
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2020-03-23 20:41:51 +0100

    moving ghost
    Signed-off-by: Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>

--- R/ghosts.R
+++ R/ghosts.R
@@ -1,13 +1,20 @@
-draw_ghost <- function(x, y, colour) {
-  ghost_arc <- tribble(
+# library(ggplot2)
+# library(ggtext)
+# library(ggforce)
+# library(tidyr)
+# library(dplyr)
+# library(purrr)
+# library(gganimate)
+
+base_ghost <- function() {
+  ghost_arc <- unnest(tribble(
     ~x0, ~y0, ~r, ~start, ~end, ~part,
     0, 0, 0.5, - 1 * pi / 2, 1 * pi / 2, "top",
     c(-1 / 6, 1 / 6), -0.5 + 1/8, 0.125, pi / 2, 3 * pi / 2, "bottom",
     -0.5, -0.5 + 1/8, 0.125, 2 * pi / 4, 4 * pi / 4, "bottom",
     0.5, -0.5 + 1/8, 0.125, - 2 * pi / 4, - 4 * pi / 4, "bottom",
     c(-1 / 3, 0, 1 / 3), -0.5 + 1/8, 1 / 24, - 1 * pi / 2, 1 * pi / 2, "bottom",
-  ) %>%
-    unnest("x0")
+  ), "x0")
 
   top <- ggplot() +
     geom_arc_bar(
@@ -27,45 +34,57 @@ draw_ghost <- function(x, y, colour) {
 
   bottom_polygon <- ggplot_build(bottom)$data[[1]][, c("x", "y")]
 
-  eyes_circle <- tribble(
-    ~x0, ~y0, ~r, ~part,
-    1/5, 1/8, 1/8, "eye",
-    -1/5, 1/8, 1/8, "eye",
-    5/20, 1/8, 1/20, "iris",
-    -3/20, 1/8, 1/20, "iris"
-  )
-
-  ghost <- bind_rows(
+  bind_rows(
     top_polygon[-nrow(top_polygon), ],
     tibble(x = 0.5, y = -0.5 + 1/8),
     bottom_polygon
   )
-
-  # ggplot() +
-  # theme_void() +
-  list(
-    geom_polygon(
-      data = ghost,
-      mapping = aes(x = x + {{ x }}, y = y + {{ y }}),
-      fill = colour,
-      colour = colour
-    ),
-    geom_circle(
-      data = filter(eyes_circle, part == "eye"),
-      mapping = aes(x0 = x0 + {{ x }}, y0 = y0+ {{ y }}, r = r),
-      colour = "white",
-      fill = "white",
-      inherit.aes = FALSE
-    ),
-    geom_circle(
-      data = filter(eyes_circle, part == "iris"),
-      mapping = aes(x0 = x0 + {{ x }}, y0 = y0+ {{ y }}, r = r),
-      colour = "black",
-      fill = "black",
-      inherit.aes = FALSE
-    )
-  )
 }
+
+eyes_circle <- tribble(
+  ~x0, ~y0, ~r, ~part,
+  1/5, 1/8, 1/8, "eye",
+  -1/5, 1/8, 1/8, "eye",
+  5/20, 1/8, 1/20, "iris",
+  -3/20, 1/8, 1/20, "iris"
+)
+
+bg <- base_ghost()
+
+red_ghost <- tribble(
+  ~colour, ~x, ~y,
+  "red", 0:10, 0
+) %>%
+  unnest(c("x", "y")) %>%
+  mutate(state = list(1:4)) %>%
+  unnest("state") %>%
+  mutate(
+    step = 1:n(),
+    noise_x = rnorm(n(), mean = 0, sd = 0.05),
+    noise_y = rnorm(n(), mean = 0, sd = 0.05),
+    body = pmap(
+      .l = list(x, y, noise_x, noise_y),
+      .f = function(.x, .y, .noise_x, .noise_y) {
+        mutate(
+          .data = bg,
+          x = x + .x + .noise_x,
+          y = y + .y + .noise_y
+        )
+      }
+    ),
+    eyes = pmap(
+      .l = list(x, y, noise_x, noise_y),
+      .f = function(.x, .y, .noise_x, .noise_y) {
+        mutate(
+          .data = eyes_circle,
+          x0 = x0 + .x + .noise_x,
+          y0 = y0 + .y + .noise_y
+        )
+      }
+    ),
+    x = NULL,
+    y = NULL
+  )
 
 p <- ggplot() +
   theme_void(base_family = "xkcd") +
@@ -75,11 +94,37 @@ p <- ggplot() +
     plot.caption.position = "plot",
     plot.background = element_rect(fill = "black", colour = "black"),
     panel.background = element_rect(fill = "black", colour = "black")
-  )  +
-  coord_fixed(xlim = c(-1.2, 1.2), ylim = c(-1.2, 1.2)) +
-  draw_ghost(-0.6, -0.6, "red") +
-  draw_ghost(0.6, -0.6, "pink") +
-  draw_ghost(-0.6, 0.6, "orange") +
-  draw_ghost(0.6, 0.6, "blue")
+  ) +
+  coord_fixed(xlim = c(-1, 11), ylim = c(-1.2, 1.2)) +
+  geom_polygon(
+    data = unnest(red_ghost, "body"),
+    mapping = aes(x = x, y = y, fill = colour, colour = colour),
+    inherit.aes = FALSE,
+    show.legend = FALSE
+  ) +
+  geom_circle(
+    data = filter(unnest(red_ghost, "eyes"), part == "eye"),
+    mapping = aes(x0 = x0, y0 = y0, r = r),
+    colour = "white",
+    fill = "white",
+    inherit.aes = FALSE,
+    show.legend = FALSE
+  ) +
+  geom_circle(
+    data = filter(unnest(red_ghost, "eyes"), part == "iris"),
+    mapping = aes(x0 = x0, y0 = y0, r = r),
+    colour = "black",
+    fill = "black",
+    inherit.aes = FALSE,
+    show.legend = FALSE
+  )
 
-ggsave(filename = "man/figures/ghosts.png", plot = p, width = 6, height = 6, units = "cm")
+animate(
+  plot = p + transition_manual(step),
+  width = 3.7 * 2.54,
+  height = 1 * 2.54,
+  units = "cm",
+  res = 120,
+  bg = "black",
+  renderer = gifski_renderer(file = "figures/ghosts.gif")
+)
