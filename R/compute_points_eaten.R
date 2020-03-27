a8579962d15fa4ec30ffb79d7dc558d1dfe63094
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2020-03-27 15:06:30 +0100

    base coordinates as dataset; rename to ggpacman
    Signed-off-by: Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>

--- R/compute_points_eaten.R
+++ R/compute_points_eaten.R
@@ -1,3 +1,9 @@
+#' Compute Point Eaten by Pac-Man
+#'
+#' @param bonus_points Bonus points coordinates.
+#' @param pacman_moves Pac-Man computed moves.
+#'
+#' @keywords internal
 compute_points_eaten <- function(bonus_points, pacman_moves) {
   dplyr::right_join(bonus_points, pacman_moves, by = c("x", "y")) %>%
     dplyr::distinct(.data[["step"]], .data[["x"]], .data[["y"]], .data[["type"]]) %>%
--- R/pacman-package.R
+++ R/pacman-package.R
@@ -1,6 +1,51 @@
 #' @keywords internal
 "_PACKAGE"
 
+#' Inky moves
+#'
+#' @format A [[`tbl`]] object.
+"inky"
+
+#' Blinky moves
+#'
+#' @format A [[`tbl`]] object.
+"blinky"
+
+#' Pinky moves
+#'
+#' @format A [[`tbl`]] object.
+"pinky"
+
+#' Clyde moves
+#'
+#' @format A [[`tbl`]] object.
+"clyde"
+
+#' Pac-Man moves
+#'
+#' @format A [[`tbl`]] object.
+"pacman"
+
+#' Ghost Body Polygon
+#'
+#' @format A [[`tbl`]] object.
+"ghost_body"
+
+#' Ghost Eyes Polygon
+#'
+#' @format A [[`tbl`]] object.
+"ghost_eyes"
+
+#' Maze Walls
+#'
+#' @format A [[`tbl`]] object.
+"maze_walls"
+
+#' Maze Points
+#'
+#' @format A [[`tbl`]] object.
+"maze_points"
+
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
